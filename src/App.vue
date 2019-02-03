7007c378ca50c873c756c6f9d64eba8fdc3a146a
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-03 10:57:55 +0800

    添加组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
 import { getSeller } from 'api'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
+import Tab from 'components/tab/tab'
 
 export default {
   name: 'app',
   data() {
     return {
         seller: {}
+    }
+  },
+  computed: {
+    tabs() {
+      return [
+        {
+          label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '评论',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
     }
   },
   created() {
@@ -26,7 +60,8 @@ export default {
     }
   },
   components: {
-    VHeader
+    VHeader,
+    Tab
   }
 }
 </script>
